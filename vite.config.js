4a0fee38cea1f4a2caac5200b5e76860f13ee7b6
ayamkv <65036385+ayamkv@users.noreply.github.com>
2023-09-03 18:37:25 +0700

    try build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+	esbuild: {
+		supported: {
+		  'top-level-await': true //browsers can handle top-level-await features
+		},
+	},
 	plugins: [
 		sveltekit(),
 	]
