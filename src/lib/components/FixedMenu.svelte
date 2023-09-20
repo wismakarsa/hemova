980ecd269af522086db2aa8a666b6b00f061edb7
ayamkv <65036385+ayamkv@users.noreply.github.com>
2023-09-20 02:51:52 +0000

    Pending changes exported from your codespace

--- src/lib/components/FixedMenu.svelte
+++ src/lib/components/FixedMenu.svelte
@@ -15,7 +15,25 @@
 	<Button {editor} key="mingcute:bold-fill" keyFunc="bold" on:click={() => toggleBold(editor)} />
 	<Button {editor} key="mingcute:italic-fill" keyFunc="italic" on:click={() => toggleItalic(editor)} />
 	<Button {editor} key="mingcute:strikethrough-fill" keyFunc="strike" on:click={() => toggleStrike(editor)} />
+	<!--
 	<Button {editor} key="mingcute:heading-1-fill" keyFunc="heading" on:click={() => toggleHeading1(editor)} />
+	<ButtonHeading {editor} key="mingcute:heading-2-fill" on:click={() => toggleHeading2(editor)} />
+	-->
+	<button class="btn btn-sm" on:click|preventDefault={() => toggleHeading1(editor)}
+        class:active={editor.isActive("heading", { level: 1 })}>
+		<iconify-icon icon="mingcute:heading-1-fill" width="14"></iconify-icon>
+	</button>
+	<button class="btn btn-sm" on:click|preventDefault={() => toggleHeading2(editor)}
+        class:active={editor.isActive("heading", { level: 2 })}>
+		<iconify-icon icon="mingcute:heading-2-fill" width="14"></iconify-icon>
+	</button>
+	<!--
+	<button class="btn btn-sm" on:click|preventDefault={() => toggleHeading3(editor)}
+        class:active={editor.isActive("heading", { level: 3 })}>
+		<iconify-icon icon="mingcute:heading-3-fill" width="14"></iconify-icon>
+	</button>
+	-->
+
 </div>
 {/if}
 
@@ -23,5 +41,13 @@
 	.fixed-menu {
 		padding: 0.50em;
 		min-height: 36px;
+	}
+	button {
+	margin: 0;
+	}
+	
+	button.active {
+		background: black;
+		color: white;
 	}
 </style>
--- src/lib/components/buttonAction.js
+++ src/lib/components/buttonAction.js
@@ -3,9 +3,9 @@ const chainer = (editor, fn) => editor.chain().focus()[fn]().run();
 const toggleBold = editor => chainer(editor, 'toggleBold');
 const toggleItalic = editor => editor.chain().focus().toggleItalic().run();
 const toggleStrike = editor => editor.chain().focus().toggleStrike().run();
-const toggleHeading1 = editor => editor.chain().focus().toggleHeading({ level: 1}).run();
-const toggleHeading2 = editor => editor.chain().focus().toggleHeading({ level: 2}).run();
-const toggleHeading3 = editor => editor.chain().focus().toggleHeading({ level: 3}).run();
+const toggleHeading1 = editor => editor.chain().focus().toggleHeading({ level: 1 }).run();
+const toggleHeading2 = editor => editor.chain().focus().toggleHeading({ level: 2 }).run();
+const toggleHeading3 = editor => editor.chain().focus().toggleHeading({ level: 3 }).run();
 
 
 
